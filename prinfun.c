ed30a7f37baeea33e929be1beb5982306caf0533
Nkyyyy <odinakafv14423@gmail.com>
2023-09-07 14:45:57 +0000

    created two new files

---
+++ prinfun.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+* _putzar - brings the character to std output
+* @c: the character to print out
+*
+* Return: on success 1
+* on error, -1 is returned, and error is set appr.
+*/
+int _putzar(char c)
+{
+return (write(1, &c, 1));
+}
+
+/**
+ * _putz - outputs a string to standard output
+ * @str: the string to be printed
+ * Return: 0 on success
+ */
+int _putz(const char *str)
+{
+	if (str == NULL)
+	{
+		return (EOF);
+	}
+	while (*str != '\0')
+	{
+		_putzar(*str);
+		str++;
+	}
+	_putzar('\n');
+
+	return (0);
+}
---
+++ strfun.c
@@ -0,0 +1,90 @@
+#include "main.h"
+/**
+ *  _strcate - function that concatenates two strings
+ *  @dest: entered value
+ *  @src: entered value
+ *
+ *  Return: void
+ */
+char *_strcate(char *dest, char *src)
+{
+		int a;
+
+		int b;
+
+		a = 0;
+
+		while (dest[a] != '\0')
+		{
+			a++;
+		}
+		b = 0;
+
+		while (src[b] != '\0')
+		{
+			dest[a] = src[b];
+			a++;
+			b++;
+		}
+		dest[a] = '\0';
+		return (dest);
+}
+
+#include "main.h"
+/**
+ * _strdupp -  returns a pointer to a new string, a duplicate
+ * @str: the original string
+ * Return: NUlL if pointer is NULL, or ponter to new string
+ */
+char *_strdupp(char *str)
+{
+	char *aaa;
+	int i, r = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	aaa = malloc(sizeof(char) * (i + 1));
+
+	if (aaa == NULL)
+	{
+		return (NULL);
+	}
+
+	for (r = 0; str[r]; r++)
+	{
+		aaa[r] = str[r];
+	}
+
+	return (aaa);
+}
+
+#include "main.h"
+/**
+ * _stricomp - compare string values
+ * @s1: input value
+ * @s2: input value
+ *
+ * Return: s1[i] - s2[i]
+ */
+int _stricomp(char *s1, char *s2)
+{
+		int i;
+
+		i = 0;
+		while (s1[i] != '\0' && s2[i] != '\0')
+		{
+			if (s1[i] != s2[i])
+			{
+				return (s1[i] - s2[i]);
+			}
+			i++;
+		}
+		return (0);
+}
